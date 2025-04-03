1d558a4275759fa1c19573809b2debc3617d0d41
eduardo <eduardo.vasquez.nolasco@telenetgroup.be>
2025-04-03 07:41:09 +0200

    set python path

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+echo "PYTHONPATH=$PYTHONPATH:$(pwd)/"
 # Load the .env file
 source .env
+
+# Set python path
+echo "PYTHONPATH=$PYTHONPATH:$(pwd)/"
 
 # Execute feature.py
 python3 ./src/pipeline/feature.py
