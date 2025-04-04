538b22ed86934e682700da52711e0918107d98a0
eduardo <eduardo.vasquez.nolasco@telenetgroup.be>
2025-04-04 08:15:02 +0200

    ci

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -4,10 +4,10 @@
 source .env
 
 # Execute feature.py
-python ./src/pipeline/feature.py
+python src/pipeline/feature.py
 
 # Execute training.py
-python ./src/pipeline/training.py
+python src/pipeline/training.py
 
 # Execute inference.py
-python ./src/pipeline/inference.py
+python src/pipeline/inference.py
